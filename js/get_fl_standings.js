adc640bd701584352ea2517da439f5383fb26a77
FootyTix <footytix.gm@gmail.com>
2020-12-06 18:44:26 +0900

    Add get_standings.js files

---
+++ js/get_fl_standings.js
@@ -0,0 +1,56 @@
+$(function () {
+    $.ajaxSetup({
+        headers: { "X-Auth-Token": "a058bef742ce4fc181380da80398a9b8" }
+    });
+    $.getJSON('https://api.football-data.org/v2/competitions/FL1/standings?standingType=TOTAL', function (data) {
+        //JSON取得後の処理
+        standings = data.standings[0].table;
+
+        var club_list = {
+            'Liverpool FC': 'リバプール',
+            'Manchester City FC': 'マンチェスター・C',
+            'Manchester United FC': 'マンチェスター・U',
+            'Arsenal FC': 'アーセナル',
+            'Tottenham Hotspur FC': 'トッテナム',
+            'Chelsea FC': 'チェルシー',
+            'Everton FC': 'エバートン',
+            'Leicester City FC': 'レスター',
+            'West Ham United FC': 'ウェストハム',
+            'Southampton FC': 'サウサンプトン',
+            'Wolverhampton Wanderers FC': 'ウォルヴァーハンプトン',
+            'Aston Villa FC': 'アストン・ヴィラ',
+            'Newcastle United FC': 'ニューカッスル',
+            'Leeds United FC': 'リーズ ',
+            'Crystal Palace FC': 'クリスタル・パレス',
+            'Brighton & Hove Albion FC': 'ブライトン',
+            'Fulham FC': 'フラム',
+            'West Bromwich Albion FC': 'WBA',
+            'Burnley FC': 'バーンリー',
+            'Sheffield United FC': 'シェフィールド'
+        };
+        // 順位表作成
+        standings.forEach(function (standing) {
+            $("#standings-tbl").append(
+                '<tr align="center">'
+                + '<td><span style="font-size: 60%;">' + standing.position + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + '<div style = "text-align: left"><div style="padding: 4px 4px 0 4px; display: table-cell; vertical-align: middle;"><img src="' 
+                + standing.team.crestUrl + '" height="24" width="24"></div><div style="display: table-cell; vertical-align: middle;">' 
+                + standing.team.name + '</div></div></span></td>'
+                + '<td><span style="font-size: 60%;">' + standing.playedGames + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + standing.won + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + standing.draw + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + standing.lost + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + ['','+'][+(standing.goalDifference > 0)] + standing.goalDifference + '</span></td>'
+                + '<td><span style="font-size: 60%;">' + standing.points + '</span></td>'
+                + '</tr>'
+            )
+        });
+        $('#loading-gif').remove();
+    })
+    .error(function () {
+        // エラーがあった時
+        $('#loading-gif').children().remove();
+        $('#loading-gif').append('ページを更新してください');
+    });
+});
+
