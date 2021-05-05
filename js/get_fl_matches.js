44fe3417c3f897b9bc0bef147c117fafc19591a5
FootyTix <footytix.gm@gmail.com>
2021-05-05 10:40:04 +0900

    add get_pd_matches.js

---
+++ js/get_fl_matches.js
@@ -0,0 +1,85 @@
+$(function () {
+    $.ajaxSetup({
+        headers: { "X-Auth-Token": "988f0be1f26f481fae9b8b19d0327312" }
+    });
+    $.getJSON('https://api.football-data.org/v2/competitions/PL/matches?status=SCHEDULED', function (data) {
+
+        //JSON取得後の処理
+        matches = data.matches;
+        matches.forEach(function (match) {
+            match.competition = data.competition
+            match.td_class = "td-cl"
+        });
+        games_num = data.count;
+
+        game_list = matches;
+
+        var club_list = {
+            'Liverpool FC': 'リバプール',
+            'Manchester City FC': 'マンチェスター・C',
+            'Manchester United FC': 'マンチェスター・U',
+            'Arsenal FC': 'アーセナル',
+            'Tottenham Hotspur FC': 'トッテナム',
+            'Chelsea FC': 'チェルシー',
+            'Everton FC': 'エバートン',
+            'Leicester City FC': 'レスター',
+            'West Ham United FC': 'ウェストハム',
+            'Southampton FC': 'サウサンプトン',
+            'Wolverhampton Wanderers FC': 'ウォルヴァーハンプトン',
+            'Aston Villa FC': 'アストン・ヴィラ',
+            'Newcastle United FC': 'ニューカッスル',
+            'Leeds United FC': 'リーズ ',
+            'Crystal Palace FC': 'クリスタル・パレス',
+            'Brighton & Hove Albion FC': 'ブライトン',
+            'Fulham FC': 'フラム',
+            'West Bromwich Albion FC': 'WBA',
+            'Burnley FC': 'バーンリー',
+            'Sheffield United FC': 'シェフィールド・U'
+        };
+
+        var youbi = ["日", "月", "火", "水", "木", "金", "土"];
+        var date, jdate;
+        var jtime = "";
+        var matchday_count = 0;
+
+        for (var i = 0; i < games_num; i++) {
+            // 日時を日本時間に変換
+            date = new Date(game_list[i].utcDate);
+            date = date.toLocaleString("ja-JP");
+            jdate = new Date(date);
+            jtime = jdate.getHours() == 9 ? '未定' : (jdate.getHours() + ':' + ("0" + jdate.getMinutes()).slice(-2));
+            
+            //節を挿入
+            if (game_list[i].matchday > matchday_count) {
+                matchday_count = game_list[i].matchday;
+                $("#matches-tbl").append(
+                    '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
+                    + '󠁢󠁥󠁮󠁧󠁿第' + matchday_count + '節'
+                    + '</span></td></tr>'
+                );
+            }
+
+            //テーブルに挿入
+            $("#matches-tbl").append(
+                '<tr align="center">'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].homeTeam.id + '.svg" height="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].homeTeam.name] + '</span></td>'
+                + '<td class="' + game_list[i].td_class + '"><span style="font-size: 65%;">'
+                + (jdate.getMonth() + 1) + '/' + jdate.getDate() + '(' + youbi[jdate.getDay()] + ')'
+                + '<br />' + jtime + '</span><br /><span style="font-size: 65%;"></span></td>'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].awayTeam.id + '.svg" height="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].awayTeam.name] + '</span></td>'
+                + '</tr>'
+            );
+        }
+        $('#loading-gif').remove();
+    })
+        .error(function () {
+            // エラーがあった時
+            $('#loading-gif').children().remove();
+            $('#loading-gif').append('ページを更新してください');
+        });
+});
+
---
+++ js/get_pd_matches.js
@@ -0,0 +1,85 @@
+$(function () {
+    $.ajaxSetup({
+        headers: { "X-Auth-Token": "fd95bddd3dd14ef2ab44cc282c062666" }
+    });
+    $.getJSON('https://api.football-data.org/v2/competitions/PD/matches?status=SCHEDULED', function (data) {
+
+        //JSON取得後の処理
+        matches = data.matches;
+        matches.forEach(function (match) {
+            match.competition = data.competition
+            match.td_class = "td-cl"
+        });
+        games_num = data.count;
+
+        game_list = matches;
+
+        var club_list = {
+            'Club Atlético de Madrid': 'アトレティコ',
+            'Real Madrid CF': 'レアル・マドリー',
+            'FC Barcelona': 'バルセロナ',
+            'Real Sociedad de Fútbol': 'レアル・ソシエダ',
+            'Villarreal CF': 'ビジャレアル',
+            'Cádiz CF': 'カディス',
+            'Sevilla FC': 'セビージャ',
+            'Granada CF': 'グラナダ',
+            'Athletic Club': 'アスレティック・ビルバオ',
+            'Elche CF': 'エルチェ',
+            'SD Eibar': 'エイバル',
+            'Deportivo Alavés': 'アラベス',
+            'Getafe CF': 'ヘタフェ',
+            'RC Celta de Vigo': 'セルタ',
+            'Valencia CF': 'バレンシア',
+            'Real Betis Balompié': 'ベティス',
+            'Levante UD': 'レバンテ',
+            'CA Osasuna': 'オサスナ',
+            'Real Valladolid CF': 'バジャドリード',
+            'SD Huesca': 'ウエスカ'
+        };
+
+        var youbi = ["日", "月", "火", "水", "木", "金", "土"];
+        var date, jdate;
+        var jtime = "";
+        var matchday_count = 0;
+
+        for (var i = 0; i < games_num; i++) {
+            // 日時を日本時間に変換
+            date = new Date(game_list[i].utcDate);
+            date = date.toLocaleString("ja-JP");
+            jdate = new Date(date);
+            jtime = jdate.getHours() == 9 ? '未定' : (jdate.getHours() + ':' + ("0" + jdate.getMinutes()).slice(-2));
+            
+            //節を挿入
+            if (game_list[i].matchday > matchday_count) {
+                matchday_count = game_list[i].matchday;
+                $("#matches-tbl").append(
+                    '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
+                    + '󠁢󠁥󠁮󠁧󠁿第' + matchday_count + '節'
+                    + '</span></td></tr>'
+                );
+            }
+
+            //テーブルに挿入
+            $("#matches-tbl").append(
+                '<tr align="center">'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].homeTeam.id + '.svg" height="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].homeTeam.name] + '</span></td>'
+                + '<td class="' + game_list[i].td_class + '"><span style="font-size: 65%;">'
+                + (jdate.getMonth() + 1) + '/' + jdate.getDate() + '(' + youbi[jdate.getDay()] + ')'
+                + '<br />' + jtime + '</span><br /><span style="font-size: 65%;"></span></td>'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].awayTeam.id + '.svg" height="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].awayTeam.name] + '</span></td>'
+                + '</tr>'
+            );
+        }
+        $('#loading-gif').remove();
+    })
+        .error(function () {
+            // エラーがあった時
+            $('#loading-gif').children().remove();
+            $('#loading-gif').append('ページを更新してください');
+        });
+});
+
