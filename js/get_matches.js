e52bab23a6fef82537a4646d4ee31254a89daa9a
FootyTix <footytix.gm@gmail.com>
2021-06-01 22:55:28 +0900

    fix get_matches

--- js/get_matches.js
+++ js/get_matches.js
@@ -38,27 +38,27 @@ $(function () {
     // });
     $.when(
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/PL/matches",
+            url: "https://api.football-data.org/v2/competitions/PL/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "28fcc697165249959737b7f980aeefd2" }
         }),
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/BL1/matches",
+            url: "https://api.football-data.org/v2/competitions/BL1/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "f70cd1be97664d618660f91a5d736505" }
         }),
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/PD/matches",
+            url: "https://api.football-data.org/v2/competitions/PD/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "d7027a3a37224b918d5af11421c478a9" }
         }),
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/SA/matches",
+            url: "https://api.football-data.org/v2/competitions/SA/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "061bce2b0738476f94d97e516179852d" }
         }),
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/FL1/matches",
+            url: "https://api.football-data.org/v2/competitions/FL1/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "3752f505d947465d8b1c5b03430890eb" }
         }),
         $.getJSON({
-            url: "https://api.football-data.org/v2/competitions/CL/matches",
+            url: "https://api.football-data.org/v2/competitions/CL/matches?status=SCHEDULED",
             headers: { "X-Auth-Token": "28d539fc326e418e930ef4221a051b1d" }
         })
     )
