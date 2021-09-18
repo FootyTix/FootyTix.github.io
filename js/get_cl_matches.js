7833171edaa842ed460fd3eb09a07ee02fc48454
FootyTix <footytix.gm@gmail.com>
2021-09-18 12:26:32 +0900

    cl down version

--- js/get_cl_matches.js
+++ js/get_cl_matches.js
@@ -2,7 +2,7 @@ $(function () {
     $.ajaxSetup({
         headers: { "X-Auth-Token": "988f0be1f26f481fae9b8b19d0327312" }
     });
-    $.getJSON('https://api.football-data.org/v2/competitions/CL/matches', function (data) {
+    $.getJSON('https://api.football-data.org/v2/competitions/CL/matches?status=SCHEDULED', function (data) {
 
         //JSON取得後の処理
         matches = data.matches;
@@ -53,32 +53,6 @@ $(function () {
         var date, jdate;
         var jtime = "";
         var matchday_count = 0;
-        var past_matchday_count = 1;
-
-        // スコア計算
-        function getScore(score, fullScore, pkScore) {
-            if (score.duration == 'PENALTY_SHOOTOUT') {
-                return (fullScore - pkScore) + '(' + pkScore + ')';
-            } else {
-                return fullScore;
-            }
-        }
-        // スコアとキックオフ時間
-        function getScoreOrDate(game, game_jdate, game_jtime) {
-            if (game.score.fullTime.homeTeam > game.score.fullTime.awayTeam) {
-                return '<td class="' + game.td_class + '"><span style="font-size: 80%; color: #ff0000;">'
-                + getScore(game.score, game.score.fullTime.homeTeam, game.score.penalties.homeTeam) + '</span><span style="font-size: 80%; color: #454545;"> - ' + getScore(game.score, game.score.fullTime.awayTeam, game.score.penalties.awayTeam) 
-                + '</span></td>';
-            } else if (game.score.fullTime.homeTeam < game.score.fullTime.awayTeam) {
-                return '<td class="' + game.td_class + '"><span style="font-size: 80%; color: #454545;">'
-                + getScore(game.score, game.score.fullTime.homeTeam, game.score.penalties.homeTeam) + ' - </span><span style="font-size: 80%; color: #ff0000;">' + getScore(game.score, game.score.fullTime.awayTeam, game.score.penalties.awayTeam) 
-                + '</span></td>';
-            } else {
-                return '<td class="' + game.td_class + '"><span style="font-size: 80%; color: #454545;">'
-                + getScore(game.score, game.score.fullTime.homeTeam, game.score.penalties.homeTeam) + ' - ' + getScore(game.score, game.score.fullTime.awayTeam, game.score.penalties.awayTeam) 
-                + '</span></td>';
-            }
-        }
 
         for (var i = 0; i < games_num; i++) {
             // 日時を日本時間に変換
@@ -86,68 +60,38 @@ $(function () {
             date = date.toLocaleString("ja-JP");
             jdate = new Date(date);
             jtime = jdate.getHours() == 9 ? '未定' : (jdate.getHours() + ':' + ("0" + jdate.getMinutes()).slice(-2));
-
-            if (game_list[i].status == 'FINISHED') {
-                //節を挿入
-                if (game_list[i].matchday != past_matchday_count) {
-                    $("#results-tbl").prepend(
-                        '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
-                        + '󠁢󠁥󠁮󠁧󠁿第' + past_matchday_count + '節'
-                        + '</span></td></tr>'
-                    );
-                    past_matchday_count = game_list[i].matchday;
-                }
-                //テーブルに挿入
-                $("#results-tbl").prepend(
-                    '<tr align="center">'
-                    + '<td style="padding-top: 6px;"><img src="https://crests.football-data.org/' + game_list[i].homeTeam.id + '.svg" height="24" width="24">'
-                    + '<br /><span style="font-size: 70%;">'
-                    + club_list[game_list[i].homeTeam.name] + '</span></td>'
-                    + getScoreOrDate(game_list[i], jdate, jtime)
-                    + '<td style="padding-top: 6px;"><img src="https://crests.football-data.org/' + game_list[i].awayTeam.id + '.svg" height="24" width="24">'
-                    + '<br /><span style="font-size: 70%;">'
-                    + club_list[game_list[i].awayTeam.name] + '</span></td>'
-                    + '</tr>'
-                );
-
-            } else {
-                //節を挿入
-                if (game_list[i].matchday != future_matchday_count) {
-                    future_matchday_count = game_list[i].matchday;
-                    $("#matches-tbl").append(
-                        '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
-                        + '󠁢󠁥󠁮󠁧󠁿第' + future_matchday_count + '節'
-                        + '</span></td></tr>'
-                    );
-                }
-                //テーブルに挿入
+            
+            //節を挿入
+            if (game_list[i].matchday != matchday_count) {
+                matchday_count = game_list[i].matchday;
                 $("#matches-tbl").append(
-                    '<tr align="center">'
-                    + '<td style="padding-top: 6px;"><img src="https://crests.football-data.org/' + game_list[i].homeTeam.id + '.svg" height="24" width="24">'
-                    + '<br /><span style="font-size: 70%;">'
-                    + club_list[game_list[i].homeTeam.name] + '</span></td>'
-                    + '<td class="' + game_list[i].td_class + '"><span style="font-size: 65%; color: #454545;">'
-                    + (jdate.getMonth() + 1) + '/' + jdate.getDate() + '(' + youbi[jdate.getDay()] + ')'
-                    + '<br />' + jtime + '</span></td>'
-                    + '<td style="padding-top: 6px;"><img src="https://crests.football-data.org/' + game_list[i].awayTeam.id + '.svg" height="24" width="24">'
-                    + '<br /><span style="font-size: 70%;">'
-                    + club_list[game_list[i].awayTeam.name] + '</span></td>'
-                    + '</tr>'
+                    '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
+                    + '󠁢󠁥󠁮󠁧󠁿第' + matchday_count + '節'
+                    + '</span></td></tr>'
                 );
             }
+
+            //テーブルに挿入
+            $("#matches-tbl").append(
+                '<tr align="center">'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].homeTeam.id + '.svg" height="24" width="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].homeTeam.name] + '</span></td>'
+                + '<td class="' + game_list[i].td_class + '"><span style="font-size: 65%;">'
+                + (jdate.getMonth() + 1) + '/' + jdate.getDate() + '(' + youbi[jdate.getDay()] + ')'
+                + '<br />' + jtime + '</span><br /><span style="font-size: 65%;"></span></td>'
+                + '<td><img src="https://crests.football-data.org/' + game_list[i].awayTeam.id + '.svg" height="24" width="24">'
+                + '<br /><span style="font-size: 80%;">'
+                + club_list[game_list[i].awayTeam.name] + '</span></td>'
+                + '</tr>'
+            );
         }
-        $("#results-tbl").prepend(
-            '<tr><td style="background-color: #1464b3; color: #ffffff;" colspan="3" align="center"><span style="font-size: 80%;">'
-            + '󠁢󠁥󠁮󠁧󠁿第' + past_matchday_count + '節'
-            + '</span></td></tr>'
-        );
         $('#loading-gif').remove();
-        $('#results-loading-gif').remove();
     })
-    .error(function () {
-        // エラーがあった時
-        $('#loading-gif').children().remove();
-        $('#loading-gif').append('ページを更新してください');
-    });
+        .error(function () {
+            // エラーがあった時
+            $('#loading-gif').children().remove();
+            $('#loading-gif').append('ページを更新してください');
+        });
 });
 
